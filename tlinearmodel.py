0936ca1ecb6c4404b920dc9a8dbd1315874cfc27
Christian Staubli <christian.staubli@gmail.com>
2018-06-14 20:57:12 +0200

    cleanup

--- tlinearmodel.py
+++ tlinearmodel.py
@@ -3,29 +3,20 @@ import tensorflow as tf
 W = tf.Variable([0.3], dtype=tf.float32)
 b = tf.Variable([-0.3], dtype=tf.float32)
 x = tf.placeholder(dtype=tf.float32)
+y = tf.placeholder(dtype=tf.float32)
 
 linear_model = W * x + b
-
-sess = tf.Session()
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
 
 init = tf.global_variables_initializer()
+sess = tf.Session()
 sess.run(init)
-
-print(sess.run(linear_model, {x: [1, 2, 3, 4, 5, 6, 7, 8, 9]}))
-
-y = tf.placeholder(dtype=tf.float32)
-squared_deltas = tf.square(linear_model - y)
-
-loss = tf.reduce_sum(squared_deltas)
 
 print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-
 train = optimizer.minimize(loss)
-
-init = tf.global_variables_initializer()
-sess.run(init)
 
 for i in range(1000):
     sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
